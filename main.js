4ec96b8bd1f427fe588021303ee17870b665fbe1
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2024-03-25 11:05:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let botao = document.querySelectorAll('.botoes')
-
-function cor(i){
-    
-    botao[i].classList.add('botaoAtivo')
+function cor(){
+    let botao= document.querySelectorAll('.botoes')
+    for(i= 0;i<botao.length;i++){
+        botao[i].onclick =function(){
+            for(j=0;j<botao.length;j++){
+                botao[j].classList.remove('ativo')
+                
+            }
+        botao[i].classList.add('botaoAtivo')
+        }
+    }
     
 }
+
+    
